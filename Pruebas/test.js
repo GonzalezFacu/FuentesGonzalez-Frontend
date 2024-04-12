856406fdb46dc6d0af466ea2d5fa67b0a437335e
Facundo Gonzalez Pelaitai <fp.gonzalez@alumno.etec.um.edu.ar>
2024-04-12 12:21:30 -0300

    Commit codigo de las ultimas clases

---
+++ Pruebas/test.js
@@ -0,0 +1,5 @@
+// Pedir un valor mediante ventana el ingreso de su edad y mostrarl por consola.
+
+let a = prompt("Edad: ");
+
+console.log(a);
---
+++ Pruebas/test2.js
@@ -0,0 +1,8 @@
+//Declarar un array vacio, ejecutar un bucle que vaya del 80 al 100 inclusive, agregar cada uno de los numeros al array y mostrar el array entero
+
+let lista = []
+
+for(i=80; i<=100; i++){
+    lista.push(i);
+}
+console.log(lista);
