6b3038472893628b0e605a635d721f9ae3a29eab
Facundo Gonzalez <95766630+GonzalezFacu@users.noreply.github.com>
2024-04-26 11:14:12 -0300

    Delete Pruebas directory

--- Pruebas/test.js
+++
@@ -1,5 +0,0 @@
-// Pedir un valor mediante ventana el ingreso de su edad y mostrarl por consola.
-
-let a = prompt("Edad: ");
-
-console.log(a);
--- Pruebas/test1.js
+++
@@ -1,7 +0,0 @@
-// Declarar en una variable la siguiente palabra "nomelavoyallevar"
-
-let palabra = "No Me La Voy A Llevar"
-
-for(i=0; i< palabra.length; i++){
-    console.log(palabra[i])
-}
